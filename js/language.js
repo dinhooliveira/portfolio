21d2c7e04d3adc8dde5423261d860232c495fa06
dinhooliveira <oliveiracienciacomputacao@gmail.com>
2020-01-09 15:59:20 -0300

    internacionalização

--- js/language.js
+++ js/language.js
@@ -8,21 +8,64 @@ export default [
             labelCourse: 'Curso',
             descriptionCourse1: "CIÊNCIA DA COMPUTAÇÃO",
             descriptionCourse2: "CIÊNCIA DE DADOS COM ÊNFASE EM BUSINESS INTELLIGENCE",
-            labelEducationalInstitution: 'Institução de Ensino'
+            labelEducationalInstitution: 'Institução de Ensino',
+            titleExperience: "Experiência profissional",
+            experience1: {
+                office: 'EMPREENDEDOR',
+                period: 'Outubro de 2019 até o momento',
+                description: 'Desenvolvimento de sites e sistemas sob demanda de clientes.'
 
+            },
+            experience2: {
+                office: 'PROGRAMADOR JUNIOR',
+                period: 'Abril 2019 até o momento',
+                description: 'Desenvolvimento de novas aplicações e manutenção de códigos em sistemas académicos.'
+            },
+            experience3: {
+                office: 'PROGRAMADOR FULLSTACK',
+                period: 'Abril de 2018 até Abril de 2019',
+                description: 'Análise de requisitos, modelagem da dados, codificação de novos sistemas e manutenção em sistemas legado.'
+            },
+            experience4: {
+                office: 'PROGRAMADOR JUNIOR',
+                period: 'Maio de 2017 até Abril de 2018',
+                description: 'Codificação e elaboração de módulos, relatórios e manutenção de software para comércio de vestuário. Apoio a equipe de suporte helpdesk em sistemas operacionais Windows e Linux.'
+            }
         }
 
     },
     {
         lang: 'en',
-        description: 'english',
+        description: 'English',
         language: {
             titleSkills: 'Skills',
             titleSchooling: 'Academic graduation / title',
             labelCourse: 'Course',
             descriptionCourse1: "COMPUTER SCIENCE",
             descriptionCourse2: "BUSINESS INTELLIGENCE EMPHASIZED DATA SCIENCE",
-            labelEducationalInstitution: 'Educational Institution'
+            labelEducationalInstitution: 'Educational Institution',
+            titleExperience: "Professional experience",
+            experience1: {
+                office: 'BUSINESSMAN',
+                period: 'October 2019 so far',
+                description: 'Development of websites and systems on demand from customers.'
+
+            },
+            experience2: {
+                office: 'JUNIOR PROGRAMMER',
+                period: 'April 2019 so far',
+                description: 'Development of new applications and code maintenance in academic systems.'
+            },
+            experience3: {
+                office: 'FULLSTACK PROGRAMMER',
+                period: 'April 2018 to April 2019',
+                description: 'Requirements analysis, data modeling, coding of new systems and maintenance on legacy systems.'
+            },
+            experience4: {
+                office: 'JUNIOR PROGRAMMER',
+                period: 'May 2017 until April 2018',
+                description: 'Coding and development of modules, reports and maintenance software for clothing trade. Support helpdesk support team on Windows and Linux operating systems.'
+            }
         }
     }
 ];
--- js/main.js
+++ js/main.js
@@ -6,6 +6,7 @@ skillsBarFill.forEach(function (element) {
     barLoading(element);
 });
 const schoolingDiv = document.getElementById("schooling");
+
 function barLoading(element) {
     const percentual = element.dataset.percentual;
     var i = 0;
@@ -19,6 +20,7 @@ function barLoading(element) {
         element.innerHTML = `${i}%`;
     }, 20);
 }
+
 const selectLanguage = document.createElement("select");
 languages.forEach(function (lang) {
     const option = document.createElement("option");
@@ -28,24 +30,44 @@ languages.forEach(function (lang) {
 });
 document.getElementById("language").appendChild(selectLanguage);
 selectLanguage.addEventListener('change', (val) => changeLanguage(val.target.value));
+
 function changeLanguage(element) {
     language = element ? element : language;
     const lang = languages.find(function (obj) {
         return obj.lang == language
     });
+
     document.getElementById("title-skill").innerText = lang.language.titleSkills;
     document.getElementById("title-schooling").innerText = lang.language.titleSchooling;
     document.querySelectorAll('.label-course').forEach((element) => {
         element.innerText = lang.language.labelCourse;
     });
-
     document.querySelectorAll('.label-educational-institution').forEach((element) => {
         element.innerText = lang.language.labelEducationalInstitution;
     });
     document.getElementById("description-course-1").innerText = lang.language.descriptionCourse1;
     document.getElementById("description-course-2").innerText = lang.language.descriptionCourse2;
+    document.getElementById("title-experience").innerText = lang.language.titleExperience;
+
+    document.getElementById("experience1-office").innerText = lang.language.experience1.office;
+    document.getElementById("experience1-period").innerText = lang.language.experience1.period;
+    document.getElementById("experience1-description").innerText = lang.language.experience1.description;
+
+    document.getElementById("experience2-office").innerText = lang.language.experience2.office;
+    document.getElementById("experience2-period").innerText = lang.language.experience2.period;
+    document.getElementById("experience2-description").innerText = lang.language.experience2.description;
+
+    document.getElementById("experience3-office").innerText = lang.language.experience3.office;
+    document.getElementById("experience3-period").innerText = lang.language.experience3.period;
+    document.getElementById("experience3-description").innerText = lang.language.experience3.description;
+
+    document.getElementById("experience4-office").innerText = lang.language.experience4.office;
+    document.getElementById("experience4-period").innerText = lang.language.experience4.period;
+    document.getElementById("experience4-description").innerText = lang.language.experience4.description;
 }
+
 const cards = document.querySelectorAll(".card");
+
 function animateCards() {
     const widthWindow = (window.screen.width - 100) / 2;
     cards.forEach(function (element, index) {
@@ -71,6 +93,7 @@ function animateCards() {
 
     });
 }
+
 var cardEnableEffect = true;
 window.addEventListener("scroll", function () {
     if (this.scrollY > (schoolingDiv.offsetTop - 400) && cardEnableEffect) {
